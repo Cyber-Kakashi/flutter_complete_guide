cbe32294093e63aa6658dd2dfeb2a3665d6ee69d
Hsevhsiv <vishvesh432@gmail.com>
2021-06-12 20:26:40 +0530

    Made a simple statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestions() {
+    setState(() {
+      if (questionIndex < 1) {
+        questionIndex += 1;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = ['Question one?', 'Question 2?'];
     return MaterialApp(
-      home: Text('stupid text!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Great title'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestions,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () {
+                print('This is a lambda function');
+                print('this will execute');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestions,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
